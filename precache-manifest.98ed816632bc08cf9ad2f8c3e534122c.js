680fe1f2175bbc78471f25557634e5d10000b428
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2020-04-25 03:45:50 +0800

    Updates

---
+++ precache-manifest.98ed816632bc08cf9ad2f8c3e534122c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17239be2b87c185c425b4c09a99cd179",
+    "url": "/memories/index.html"
+  },
+  {
+    "revision": "0f761160953100ae7f8e",
+    "url": "/memories/static/css/2.38138f72.chunk.css"
+  },
+  {
+    "revision": "1e5fc6d1264913f41e08",
+    "url": "/memories/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f761160953100ae7f8e",
+    "url": "/memories/static/js/2.4022565e.chunk.js"
+  },
+  {
+    "revision": "205d16c5470d1653024f11eaab341813",
+    "url": "/memories/static/js/2.4022565e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5fc6d1264913f41e08",
+    "url": "/memories/static/js/main.2a5db175.chunk.js"
+  },
+  {
+    "revision": "68fb2d746e58e2b6fca5",
+    "url": "/memories/static/js/runtime-main.7e7a8637.js"
+  }
+]);
