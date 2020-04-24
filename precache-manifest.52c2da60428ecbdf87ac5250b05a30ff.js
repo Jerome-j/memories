006567c7bd855374771eb41554def62c1cf93a61
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2020-04-25 03:57:48 +0800

    Updates

---
+++ precache-manifest.52c2da60428ecbdf87ac5250b05a30ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f27f70cb47662a5d22bd0de7cefbe5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee71eaf2523937c9ec08",
+    "url": "/static/css/2.38138f72.chunk.css"
+  },
+  {
+    "revision": "84da125fa4eb8a16ca2c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee71eaf2523937c9ec08",
+    "url": "/static/js/2.25dbb8a5.chunk.js"
+  },
+  {
+    "revision": "205d16c5470d1653024f11eaab341813",
+    "url": "/static/js/2.25dbb8a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84da125fa4eb8a16ca2c",
+    "url": "/static/js/main.8fd8b32b.chunk.js"
+  },
+  {
+    "revision": "fed433733f2da7bd699a",
+    "url": "/static/js/runtime-main.7e600b9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/memories/precache-manifest.27ca1bf8d77693d0c31e2eca8ac89e93.js"
+  "/precache-manifest.52c2da60428ecbdf87ac5250b05a30ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memories/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
